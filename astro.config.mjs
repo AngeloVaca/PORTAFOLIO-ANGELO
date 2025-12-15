0a8bc15af49372cac6e019baf4bdda38a5ef875a
AngeloVaca <arielvaca2002@gmail.com>
2025-12-14 20:36:40 -0500

    fix: configuracion storyblok y vite funcionando

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import { storyblok } from '@storyblok/astro';
+import { loadEnv } from 'vite';
 
-// https://astro.build/config
-export default defineConfig({});
+// Esto carga tu archivo .env sin necesitar librerías extra
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN, // Aquí lee tu clave segura
+      components: {
+        // Aquí registraremos tus componentes pronto
+      },
+    })
+  ],
+});

959417e43e477ac545670265c07f4e3848ad11ae
AngeloVaca <arielvaca2002@gmail.com>
2026-01-04 20:23:12 -0500

    feat: colores acordes a tema de figma

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,19 +10,21 @@ import tailwindcss from '@tailwindcss/vite';
 
 
 
+import react from '@astrojs/react';
+
+
+
+
 // Esto carga tu archivo .env sin necesitar librerías extra
 const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN, // Aquí lee tu clave segura
-      components: {
-        page: 'storyblok/Page',
-      },
-    })
-  
-  ],
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN, // Aquí lee tu clave segura
+    components: {
+      page: 'storyblok/Page',
+    },
+  }), react()],
 
   vite: {
     plugins: [tailwindcss()],
